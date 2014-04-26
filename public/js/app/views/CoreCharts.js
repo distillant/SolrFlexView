c1ee87927f36e16e301f699af27223350f268605
distillant <pconroy@distillant.com>
2014-04-26 13:45:38 -0400

    altered charting code to utilize collection instead of mock object.

--- public/js/app/views/CoreCharts.js
+++ public/js/app/views/CoreCharts.js
@@ -15,27 +15,22 @@ return Backbone.View.extend({
     initialize: function(options){
         this.$el=options.el;
         this.$el.html(template());
-        var bob = {"responseHeader": {    "status": 0, "QTime": 125    }, "defaultCoreName": "CCVFR", "initFailures": {}, "status": {    "CCPaper": {        "name": "CCPaper", "isDefaultCore": false, "instanceDir": "E:\\SolrIndexes\\CCPaper\\", "dataDir": "E:\\SolrIndexes\\CCPaper\\data\\", "config": "solrconfig.xml", "schema": "schema.xml", "startTime": "2013-11-18T14:54:38.796Z", "uptime": 89766, "index": {            "numDocs": 2214733, "maxDoc": 2214733, "deletedDocs": 0, "version": 1025, "segmentCount": 22, "current": true, "hasDeletions": false, "directory": "org.apache.lucene.store.NRTCachingDirectory:NRTCachingDirectory(org.apache.lucene.store.SimpleFSDirectory@E:\\SolrIndexes\\CCPaper\\data\\index lockFactory=org.apache.lucene.store.NativeFSLockFactory@176ee6; maxCacheMB=48.0 maxMergeSizeMB=4.0)", "userData": {                "commitTimeMSec": "1370368323343"            }, "lastModified": "2013-06-04T17:52:03.343Z", "sizeInBytes": 717520154, "size": "684.28 MB"        }    }, "CCVFR": {        "name": "CCVFR", "isDefaultCore": true, "instanceDir": "E:\\SolrIndexes\\CCVFR\\", "dataDir": "E:\\SolrIndexes\\CCVFR\\data\\", "config": "solrconfig.xml", "schema": "schema.xml", "startTime": "2013-11-18T14:54:38.781Z", "uptime": 89797, "index": {            "numDocs": 78919, "maxDoc": 78919, "deletedDocs": 0, "version": 25, "segmentCount": 2, "current": true, "hasDeletions": false, "directory": "org.apache.lucene.store.NRTCachingDirectory:NRTCachingDirectory(org.apache.lucene.store.SimpleFSDirectory@E:\\SolrIndexes\\CCVFR\\data\\index lockFactory=org.apache.lucene.store.NativeFSLockFactory@71d29a; maxCacheMB=48.0 maxMergeSizeMB=4.0)", "userData": {                "commitTimeMSec": "1369861365734"            }, "lastModified": "2013-05-29T21:02:45.734Z", "sizeInBytes": 33955377, "size": "32.38 MB"        }    }, "CCEdata": {        "name": "CCEdata", "isDefaultCore": false, "instanceDir": "E:\\SolrIndexes\\CCEdata\\", "dataDir": "E:\\SolrIndexes\\CCEdata\\data\\", "config": "solrconfig.xml", "schema": "schema.xml", "startTime": "2013-11-18T14:54:38.89Z", "uptime": 89688, "index": {            "numDocs": 6568690, "maxDoc": 6568690, "deletedDocs": 0, "version": 9153, "segmentCount": 26, "current": true, "hasDeletions": false, "directory": "org.apache.lucene.store.NRTCachingDirectory:NRTCachingDirectory(org.apache.lucene.store.SimpleFSDirectory@E:\\SolrIndexes\\CCEdata\\data\\index lockFactory=org.apache.lucene.store.NativeFSLockFactory@b98a06; maxCacheMB=48.0 maxMergeSizeMB=4.0)", "userData": {                "commitTimeMSec": "1370006660187"            }, "lastModified": "2013-05-31T13:24:20.187Z", "sizeInBytes": 19781982159, "size": "18.42 GB"        }    }}};
-        console.log(bob);
 
-        console.log(bob.status.CCEdata.index.numDocs);
         var coreStats=[];
-        for (var item in bob.status)
-        {
-            var core= bob.status[item];
-            var coreStat ={}
-            coreStat.name=item;
-            coreStat.numDocs= core.index.numDocs;
-            coreStat.sizeInMB=(core.index.sizeInBytes/Math.pow(1024,2));
-            coreStat.instanceDir=core.instanceDir;
-            coreStat.size=core.index.size;
+        options.collection.each(function(model){
+
+            var coreStat ={};
+            coreStat.name=model.get("name");
+            coreStat.numDocs= model.get("index").numDocs;
+            coreStat.sizeInMB=((model.get("index").sizeInBytes)/Math.pow(1024,2));
+            coreStat.instanceDir=model.get("instanceDir");
+            coreStat.size=model.get("index").size;
             coreStats.push(coreStat);
-        };
+        });
 
-
-        BarChart({
-            margin: {top: 20, right: 20, bottom: 30,left: 60},
-            target: "#CoreDocsChart",
+        var chart1 = {
+        margin: {top: 20, right: 20, bottom: 30,left: 60},
+        target: "#CoreDocsChart",
             totalWidth: 500,
             totalHeight: 400,
             data: coreStats,
@@ -43,8 +38,9 @@ return Backbone.View.extend({
             yField: "numDocs",
             xField: "name",
             Yticks: 10
-        });
-        BarChart({
+         };
+
+        var chart2 ={
             margin: {top: 20, right: 20, bottom: 30,left: 60},
             target: "#IndexSizeChart",
             totalWidth: 500,
@@ -55,6 +51,8 @@ return Backbone.View.extend({
             xField: "name",
             Yticks: 10
         });
+        BarChart(chart1);
+        BarChart(chart2);
     }
 });
 });
