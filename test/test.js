0cde2f03ea756a103bf473c143ab69d8181bf187
Patrick Conroy <pconroy@distillant.com>
2014-07-09 19:02:14 -0400

    refactored to clean up tests

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('request'),
   assert = require('assert');
 
 var coreJson; //store core json feed here for further use;
-var core;
+
 describe('API UNIT TESTING- Core feed', function () {
   // GET properties
   describe('GET localhost:3000/coreList', function () {
@@ -22,12 +22,12 @@ describe('API UNIT TESTING- Core feed', function () {
     // GET properties
     describe('Core feed', function () {
         it("typeof body should equal string", function (done) {
-            request('http://localhost:3000/coreList', function (err, resp, body) {
-                assert.equal(typeof(body), "string");
-                if(typeof(body)!="undefined")
-                {
-                    coreJson=JSON.parse(body);
-                }
+                request('http://localhost:3000/coreList', function (err, resp, body) {
+                    assert.equal(typeof(body), "string");
+                   /* if(typeof(body)!="undefined")
+                    {
+                        coreJson=JSON.parse(body);
+                    }*/
                 done();
             });
         });
@@ -39,10 +39,10 @@ describe('API UNIT TESTING- Core feed', function () {
         it("JSON.Parse(body) creates a valid object", function (done) {
             request('http://localhost:3000/coreList', function (err, resp, body) {
 
-                if(typeof(body)!="undefined")
-                {
-                    coreJson=JSON.parse(body);
-                }
+               // if(typeof(body)!="undefined")
+               // {
+               coreJson=JSON.parse(body);
+              //  }
 
                 assert.equal(typeof(coreJson),"object" );
 
@@ -55,13 +55,15 @@ describe('API UNIT TESTING- Core feed', function () {
         it("check that cores list has more than 1 item", function (done) {
             assert.equal((coreJson.length > 0),true );
             done();
-            if (coreJson.length > 0)
-                 core= coreJson[0];
+
         });
     });
     describe('Core feed', function () {
         it("check that core has a name", function (done) {
+            if (coreJson.length > 0)
+            var core= coreJson[0];
             assert.equal(typeof(core.name) == "undefined", false  );
+
             done();
         });
     });
@@ -74,6 +76,7 @@ describe('API UNIT TESTING- fields field', function () {
 
     describe('GET localhost:3000/fields/:core', function () {
         it("get fields list for core", function (done) {
+            var core= coreJson[0];
             request('http://localhost:3000/fields/'+core.name, function (err, resp) {
                 assert.equal(resp.statusCode, 200);
                 done();
